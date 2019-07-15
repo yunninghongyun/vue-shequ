9331314c7d55d6ba96e46220a372ccab2c4cc6c7
yunninghongyun <786898343@qq.com>
2019-07-15 14:46:10 +0800

    vue-shequ

---
+++ src/components/Header.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="header">
+    <div class="header-inner">
+      <router-link to="/" class="logo">
+        <img src="../assets/logo.png" alt />
+        <h1>Vue.js</h1>
+      </router-link>
+      <router-link to="/topic/create" v-if="userInfo" class="huati">发布话题</router-link>
+      <router-link to="/topic/xiaoxi" v-if="userInfo" class="xiaoxi">未读消息</router-link>
+      <div v-if="!userInfo" class="login">
+        <input type="text" v-model="text" />
+        <button @click="login">登录</button>
+      </div>
+      <div v-else class="logout">
+        <img :src="userInfo.avatar_url" alt />
+        <button @click="logout">退出</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "tou",
+  data() {
+    return {
+      text: "d2263004-376e-44a6-9304-a303b526ba88",
+      userInfo: null
+    };
+  },
+  created() {
+    if (sessionStorage.getItem("token")) {
+      axios
+        .post("https://www.vue-js.com/api/v1/accesstoken", {
+          accesstoken: sessionStorage.getItem("token")
+        })
+        .then(res => {
+          this.userInfo = res.data;
+        });
+    }
+  },
+  methods: {
+    login() {
+      axios
+        .post("https://www.vue-js.com/api/v1/accesstoken", {
+          accesstoken: this.text
+        })
+        .then(res => {
+          this.userInfo = res.data;
+          console.log(res.data.id);
+          sessionStorage.setItem("token", this.text);
+          sessionStorage.setItem("user_id", res.data.id);
+        });
+    },
+    logout() {
+      this.userInfo = null;
+      sessionStorage.removeItem("token");
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.header {
+  width: 100%;
+  height: 55px;
+  background-color: #1c6132;
+  margin-bottom: 20px;
+}
+.header-inner {
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.header-inner > .logo {
+  display: flex;
+  align-items: center;
+  height: 55px;
+  color: #fff;
+}
+.header-inner > .logo > img {
+  width: 45px;
+  height: 45px;
+  margin-right: 10px;
+}
+.header-inner > .logo > h1 {
+  font-size: 24px;
+  margin: 0;
+}
+.header-inner .logout {
+  display: flex;
+  align-items: center;
+}
+.header-inner .logout img {
+  width: 40px;
+}
+.header-inner .logout button {
+  margin-left: 5px;
+  align-self: flex-end;
+}
+.header-inner .huati {
+  background-color: yellowgreen;
+  padding: 4px 8px;
+  border-radius: 2px;
+  border: none;
+}
+.header-inner .xiaoxi {
+  color: #fff;
+}
+</style>
---
+++ src/components/Topic.vue
@@ -0,0 +1,317 @@
+<template>
+  <div v-if="topic" class="topic">
+    <article>
+      <div class="article-head">
+        <div>
+          <span class="tab active" v-if="topic.top || topic.good">{{topic.top? '置顶' :'精华' }}</span>
+          <h2>{{topic.title}}</h2>
+          <span
+            v-if="isLogin"
+            @click="changeCollect"
+            class="collect"
+          >{{is_collect ? '取消收藏' : '加入收藏'}}</span>
+        </div>
+        <p style="font-size: 12px;">
+          <span>
+            <b>·</b>
+            发布于{{myMoment(topic.create_at)}}
+          </span>
+          <span>
+            <b>·</b>
+            作者 {{topic.author.loginname}}
+          </span>
+          <span>
+            <b>·</b>
+            {{topic.visit_count}} 次浏览
+          </span>
+          <span>
+            <b>·</b>
+            来自 {{topic.tab === 'share' ? '分享' : topic.tab ==='ask' ? '问答' : topic.tab ==='job' ? '招聘' : 'weex'}}
+          </span>
+        </p>
+      </div>
+      <div class="topic_content" v-html="topic.content"></div>
+    </article>
+    <div class="comment">
+      <p>{{topic.replies.length}}回复</p>
+      <ul>
+        <li v-for=" comment in topic.replies" :key="comment.id">
+          <img :src="comment.author.avatar_url" alt />
+          <div>
+            <span>{{comment.author.loginname}}</span>
+            <span>{{myMoment(comment.create_at)}}</span>
+            <span v-html="comment.content" class="content"></span>
+            <span>
+              <span @click="up(comment.id)">{{isUped(comment.id)?'👍':'赞'}}</span>
+              <span @click="addReply( comment.author.loginname)">回复</span>
+              {{comment.ups.length ? comment.ups.length:''}}
+            </span>
+          </div>
+        </li>
+      </ul>
+      <div class="comment-form">
+        <p>添加回复</p>
+        <textarea v-model="text"></textarea>
+        <button @click="addComment" class="reply">回复</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import moment from "moment";
+export default {
+  name: "topic",
+  data() {
+    return {
+      topic: null,
+      is_collect: false,
+      text: ""
+    };
+  },
+  computed: {
+    isLogin() {
+      return Boolean(sessionStorage.getItem("token"));
+    }
+  },
+  created() {
+    // 获取路由的动态参数     $route
+    const { id } = this.$route.params;
+    axios.get(`https://www.vue-js.com/api/v1/topic/${id}`).then(res => {
+      console.log(res.data.data);
+      this.topic = res.data.data;
+    });
+  },
+  methods: {
+    myMoment(time) {
+      moment.locale("zh-cn");
+      return moment(time).fromNow();
+    },
+    changeCollect() {
+      const obj = {
+        topic_id: this.$route.params.id,
+        accesstoken: sessionStorage.getItem("token")
+      };
+      if (this.is_collect) {
+        axios
+          .post(`https://www.vue-js.com/api/v1/topic/de_collect`, obj)
+          .then(res => {
+            if (res.data.success) {
+              this.is_collect = false;
+            }
+          });
+      } else {
+        axios
+          .post(`https://www.vue-js.com/api/v1/topic/collect`, obj)
+          .then(res => {
+            if (res.data.success) {
+              this.is_collect = true;
+            }
+          });
+      }
+    },
+    addComment() {
+      //
+      axios
+        .post(`https://www.vue-js.com/api/v1/topic/${this.topic.id}/replies`, {
+          accesstoken: sessionStorage.getItem("token"),
+          content: this.text
+        })
+        .then(res => {
+          // 发送请求成功之后要更新本地的 评论  但是请求的结果只是一个  评论 id 要更新本地的话需要 创建一个评论对象
+          // 所以我们换另一个方案 重新请求整个文章数据
+          axios
+            .get(`https://www.vue-js.com/api/v1/topic/${this.topic.id}`)
+            .then(res => {
+              this.topic = res.data.data;
+            });
+        });
+    },
+    up(id) {
+      // 发请求
+      // reply_id
+      if (sessionStorage.getItem("token")) {
+        axios
+          .post(`https://www.vue-js.com/api/v1/reply/${id}/ups`, {
+            accesstoken: sessionStorage.getItem("token")
+          })
+          .then(res => {
+            // 返回一个对象   {successs:true,action: 'down'}
+            // 要根据 action 的值，更新本地的展示内容(topic)
+            console.log(res.data);
+            if (res.data.action === "up") {
+              // 此时是你之前没点过，点了之后变成 up
+              // 要更新 topic 内的 replies 中的某一个评论下的 ups 数组
+              this.topic.replies
+                .find(item => item.id === id)
+                .ups.push(sessionStorage.getItem("user_id"));
+            } else {
+              this.topic.replies.find(
+                item => item.id === id
+              ).ups = this.topic.replies
+                .find(item => item.id === id)
+                .ups.filter(item => item != sessionStorage.getItem("user_id"));
+            }
+          });
+      } else {
+        alert("请登录");
+      }
+    },
+    isUped(id) {
+      // 刚进页面的时候看评论是否被用户点赞了
+      return (
+        this.topic.replies
+          .find(item => item.id === id)
+          .ups.indexOf(sessionStorage.getItem("user_id")) !== -1
+      );
+    },
+    addReply(loginname) {
+      this.text = `@${loginname}`;
+      document.querySelector("textarea").focus();
+    }
+  }
+};
+</script>
+
+<style>
+.topic {
+  width: 80%;
+  margin: 0 auto;
+}
+.topic article {
+  border-radius: 3px;
+  background-color: #fff;
+  margin-bottom: 10px;
+}
+.topic .article-head {
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+}
+.topic .article-head > div {
+  display: flex;
+  align-items: flex-end;
+}
+.topic .article-head h2 {
+  margin: 8px 0 0 0;
+  flex-grow: 1;
+}
+.topic .topic_content {
+  padding: 10px;
+  border-top: 1px solid #e5e5e5;
+}
+.markdown-text > :first-child,
+.preview > :first-child {
+  margin-top: 0;
+}
+.preview h1,
+.preview h2,
+.preview h3,
+.preview h4,
+.preview h5,
+.preview h6,
+.reply_area h1,
+.reply_area h2,
+.reply_area h3,
+.reply_area h4,
+.reply_area h5,
+.reply_area h6,
+.topic_content h1,
+.topic_content h2,
+.topic_content h3,
+.topic_content h4,
+.topic_content h5,
+.topic_content h6 {
+  margin: 30px 0 15px;
+  border-bottom: 1px solid #eee;
+}
+.preview p,
+.reply_content p,
+.reply_form p,
+.topic_content p {
+  font-size: 15px;
+  line-height: 1.7em;
+  overflow: auto;
+}
+.markdown-text p,
+.preview p {
+  white-space: pre-wrap;
+  white-space: -moz-pre-wrap;
+  white-space: -pre-wrap;
+  white-space: -o-pre-wrap;
+  word-wrap: break-word;
+  line-height: 2em;
+  margin: 1em 0;
+}
+.topic_content img {
+  height: auto;
+  max-width: 100%;
+  vertical-align: middle;
+  border: 0;
+  -ms-interpolation-mode: bicubic;
+}
+.comment-form > textarea {
+  width: 613px;
+  height: 274px;
+  resize: none;
+  border-radius: 0 0 3px 3px;
+}
+.comment > p,
+.comment-form p {
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  background-color: #f6f6f6;
+  margin: 0;
+  padding: 10px;
+  font-size: 14px;
+}
+.comment-form .reply {
+  display: inline-block;
+  float: none;
+  padding: 3px 10px;
+  border: 0;
+  margin: 0;
+  font-size: 14px;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  letter-spacing: 2px;
+  box-shadow: none;
+  border-radius: 3px;
+  line-height: 2em;
+  vertical-align: middle;
+  color: #fff;
+  background-color: #3374de;
+}
+.comment ul {
+  background-color: #fff;
+  margin: 0;
+}
+.comment ul > li {
+  list-style: none;
+  padding: 10px 0 10px 10px;
+  border-bottom: 1px solid #ccc;
+}
+.comment ul > li {
+  display: flex;
+  align-items: flex-start;
+  font-size: 12px;
+}
+.comment ul li img {
+  width: 30px;
+  height: 30px;
+  border-radius: 3px;
+  margin-right: 10px;
+}
+.comment ul li div > span:nth-child(2) {
+  color: #000;
+  font-weight: bold;
+  margin-left: 10px;
+}
+.comment ul li div > span:nth-child(3) {
+  color: #08c;
+}
+.comment ul li div > .content {
+  font-size: 14px;
+}
+</style>
